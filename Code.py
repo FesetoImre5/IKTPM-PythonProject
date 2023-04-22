18869b3608ba044e46b878af6880654d84b063ef
FesetoImre5 <feseto.imre@diak.szbi-pg.hu>
2023-04-22 19:40:28 +0200

    Continuing maze
    I'm switching editors, because Visual Studio is behaving weird

--- Code.py
+++ Code.py
@@ -45,7 +45,88 @@ def init_maze(width,height):
     maze[starting_height][starting_width - 1] = wall
     maze[starting_height][starting_width + 1] = wall
     maze[starting_height + 1][starting_width] = wall
-    return maze
+
+    while walls:
+        rand_wall = walls[int(random.random()*len(walls))-1]
+
+        if rand_wall[1] != 0:
+            if maze[rand_wall[0]][rand_wall[1]-1] == 'u' and maze[rand_wall[0]][rand_wall[1]+1] == 'c':
+                s_cells = surroundingCells(rand_wall)
+                if s_cells < 2:
+                    maze[rand_wall[0]][rand_wall[1]] = 'c'
+                
+                    if (rand_wall[0] != 0):
+                        if (maze[rand_wall[0]-1][rand_wall[1]] != 'c'):
+                            maze[rand_wall[0]-1][rand_wall[1]] = 'w'
+                        if ([rand_wall[0]-1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]-1, rand_wall[1]])
+                delete_wall(rand_wall)
+                continue
+            continue
+
+        if rand_wall[0] != 0:
+            if maze[rand_wall[0]-1][rand_wall[1]] == 'u' and maze[rand_wall[0]+1][rand_wall[1]+1] == 'c':
+                s_cells = surroundingCells(rand_wall)
+                if s_cells < 2:
+                    maze[rand_wall[0]][rand_wall[1]] = 'c'
+                
+                    if (rand_wall[0] != 0):
+                        if (maze[rand_wall[0]-1][rand_wall[1]] != 'c'):
+                            maze[rand_wall[0]-1][rand_wall[1]] = 'w'
+                        if ([rand_wall[0]-1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]-1, rand_wall[1]])
+                delete_wall(rand_wall)
+                continue
+            continue
+
+        if rand_wall[0] != height-1:
+            if maze[rand_wall[0]+1][rand_wall[1]] == 'u' and maze[rand_wall[0]-1][rand_wall[1]] == 'c':
+                s_cells = surroundingCells(rand_wall)
+                if s_cells < 2:
+                    maze[rand_wall[0]][rand_wall[1]] = 'c'
+                
+                    if (rand_wall[0] != 0):
+                        if (maze[rand_wall[0]-1][rand_wall[1]] != 'c'):
+                            maze[rand_wall[0]-1][rand_wall[1]] = 'w'
+                        if ([rand_wall[0]-1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]-1, rand_wall[1]])
+                delete_wall(rand_wall)
+                continue
+            continue
+                            
+        if rand_wall[1] != width-1:
+            if maze[rand_wall[0]][rand_wall[1]+1] == 'u' and maze[rand_wall[0]][rand_wall[1]-1] == 'c':
+                s_cells = surroundingCells(rand_wall)
+                if s_cells < 2:
+                    maze[rand_wall[0]][rand_wall[1]] = 'c'
+                
+                    if (rand_wall[0] != 0):
+                        if (maze[rand_wall[0]-1][rand_wall[1]] != 'c'):
+                            maze[rand_wall[0]-1][rand_wall[1]] = 'w'
+                        if ([rand_wall[0]-1, rand_wall[1]] not in walls):
+                            walls.append([rand_wall[0]-1, rand_wall[1]])
+                delete_wall(rand_wall)
+                continue
+            continue
+
+    def surroundingCells(rand_wall):
+        s_cells = 0
+        if (maze[rand_wall[0]-1][rand_wall[1]] == 'c'):
+            s_cells += 1
+        if (maze[rand_wall[0]+1][rand_wall[1]] == 'c'):
+            s_cells += 1
+        if (maze[rand_wall[0]][rand_wall[1]-1] == 'c'):
+            s_cells +=1
+        if (maze[rand_wall[0]][rand_wall[1]+1] == 'c'):
+            s_cells += 1
+        return s_cells
+
+    def delete_wall(rand_wall):
+        for wall in walls:
+            if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
+                walls.remove(wall)
+
+        return maze
 
 def print_maze(maze):
     for i in range(0, len(maze)):
