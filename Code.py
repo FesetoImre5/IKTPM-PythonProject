73b65d3bfc86c0da24444bdd3ba86316caac3742
xXImrePVPXx <118980968+FesetoImre5@users.noreply.github.com>
2023-05-03 08:09:38 +0200

    Revert "Meh"
    This reverts commit 673ce806924b081f48a89db603e17d39b7918604.

--- Code.py
+++ Code.py
@@ -1,40 +1,63 @@
-# két koordináta különbségéből megmondja, hogy a mező 4db boolean értékéből melyik irányú falon megyünk át
-def whichNeighbor(dx,dy):
-    return (1 if dx+dy > 0 else 0) + (2 if dx != 0 else 0)
+import random
 
-# kirajzolja a mezőket a pygame-be
-def drawMaze():
-    # feketével töltjük ki a képernyőt
-    screen.fill((0,0,0))
-    # végig megyünk a mezőkön
-    for x in range(len(maze)):
-        for y in range(len(maze[x])):
-            # a kiválasztot mezőt...
-            m = maze[x][y]
-            # ha van alja kirajzoljuk
-            if m[1]:
-                pygame.draw.line(screen, (255,255,255), (x100, (y+1)100), ((x+1)100, (y+1)100), 1)
-            # ha van jobb oldala kirajzoljuk
-            if m[3]:
-                pygame.draw.line(screen, (255, 255, 255), ((x+1)100, y100), ((x+1)100, (y+1)100), 1)
-            # a másik kettőt nem kell, mert a felettünk és balra lévőnél már rajzoltuk
+# hozzon létre egy labirintust adott szélességgel és magassággal
+def create_maze(width, height):
+    # hozzon létre egy rácsot az összes falat
+    maze = [[{"top": True, "right": True, "bottom": True, "left": True} for j in range(width)] for i in range(height)]
 
-# elindítjuk a labirintus generálását 0,0 mezőtől, innen már rekurzívan meg fogja hívni magát a többi mezőre
-genMaze(0,0)
+    # véletlen cellából indul
+    current_i, current_j = random.randint(0, height - 1), random.randint(0, width - 1)
+    stack = [(current_i, current_j)]
 
-# kirajzoljuk a legenerált labirintust, amíg a felhasználó be nem zárja
-done = False
-while not done:
-    # várjuk az X gomb megnyomását
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-            break
-    # kirajzolunk
-    drawMaze()
-    # kirakjuk a friss rajzot a képernyőre (rajzolás mindig egy másodlagos képernyőre történik, hogy ne villogjon)
-    pygame.display.flip()
-    # várunk picit, hogy kb 60fps-el rajzoljunk (ne terheljük túl a videókártyát)
-    clock.tick(60)
-# kilépett a felhasználó, pygame leállítás és vége
-pygame.quit()
+    # végezzen mélységi keresést a falak eltávolításához és utak létrehozásához
+    while stack:
+        current_i, current_j = stack.pop()
+        maze[current_i][current_j]["visited"] = True
+        directions = []
+        if current_i > 0 and not maze[current_i - 1][current_j].get("visited"):
+            directions.append(("top", -1, 0))
+        if current_j < width - 1 and not maze[current_i][current_j + 1].get("visited"):
+            directions.append(("right", 0, 1))
+        if current_i < height - 1 and not maze[current_i + 1][current_j].get("visited"):
+            directions.append(("bottom", 1, 0))
+        if current_j > 0 and not maze[current_i][current_j - 1].get("visited"):
+            directions.append(("left", 0, -1))
+        if not directions:
+            continue
+        direction, di, dj = random.choice(directions)
+        next_i, next_j = current_i + di, current_j + dj
+        maze[current_i][current_j][direction] = False
+        maze[next_i][next_j][{"top": "bottom", "right": "left", "bottom": "top", "left": "right"}[direction]] = False
+        stack.append((next_i, next_j))
+
+    # távolítsa el a látogatott zászlót
+    for i in range(height):
+        for j in range(width):
+            if "visited" in maze[i][j]:
+                del maze[i][j]["visited"]
+
+    return maze
+
+def print_maze(maze):
+    height, width = len(maze), len(maze[0])
+    for i in range(height):
+        # felső falak nyomtatása
+        for j in range(width):
+            print("+", end="")
+            print("  " if maze[i][j]["top"] else "--", end="")
+        print("+")
+        # nyomtasson bal és jobb falakat
+        for j in range(width):
+            print("|" if maze[i][j]["left"] else " ", end="")
+            print("  ", end="")
+            print("|" if maze[i][j]["right"] else " ", end="")
+        print("|")
+    # nyomtasson alsó falakat
+    for j in range(width):
+        print("+", end="")
+        print("  " if maze[height - 1][j]["bottom"] else "--", end="")
+    print("+")
+
+# példahasználat
+maze = create_maze(10, 10)
+print_maze(maze)
